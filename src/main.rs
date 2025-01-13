e6df25c0bf9b8881c7addcb21265a77c11505761
freedom-pep <freedom-pep@proton.me>
2025-01-12 18:15:46 -0600

    Create two wallets and persist

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 mod utils;
 use bdk_wallet::bitcoin::Network;
 
+const STOP_GAP: usize = 50;
+const PARALLEL_REQUESTS: usize = 1;
+const DB_PATH_1: &str = "./sqlite_db_wallet";
+const DB_PATH_2: &str = "./sqlite_db_wallet_2";
 const SEED_PHRASE_1: &str =
     "execute grunt bullet spawn panther until paper receive prison midnight tower orphan";
 const SEED_PHRASE_2: &str =
-    "palm outer embrace test escape spend monster lunar inject taxi derive jealous";
+    "rebel secret wide garment post onion amazing push inherit record exotic fold";
 const NETWORK: Network = Network::Signet;
 
 fn main() {
     println!("Start\n");
-    let seed = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_1);
-    let (descriptor, change_descriptor) = utils::create_descriptor(seed);
+    let seed_1 = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_1);
+    let wallet_descriptor = utils::create_descriptor(seed_1);
+    let (ext_descriptor, int_descriptor) = wallet_descriptor.descriptor_string();
 
-    println!(
-        "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\n",
-        descriptor, // privkey
-        change_descriptor,
-    );
+    // println!(
+    //     "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\n",
+    //     ext_descriptor, int_descriptor,
+    // );
+    let mut wallet_1 = utils::get_wallet(wallet_descriptor, DB_PATH_1);
+    // println!("address: {}", utils::get_address(&mut wallet_1, DB_PATH_1).address);
+    println!("wallet amount: {}", wallet_1.balance().total().to_sat());
+
+    let seed_2 = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_2);
+    let wallet_descriptor = utils::create_descriptor(seed_2);
+    let (ext_descriptor, int_descriptor) = wallet_descriptor.descriptor_string();
+
+    // println!(
+    //     "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\n",
+    //     ext_descriptor, int_descriptor,
+    // );
+    let mut wallet_2 = utils::get_wallet(wallet_descriptor, DB_PATH_2);
+    // println!("address: {}", utils::get_address(&mut wallet_1, DB_PATH_2).address);
+    println!("wallet amount: {}", wallet_1.balance().total().to_sat());
     println!("\nEnd");
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,18 @@
-use super::NETWORK;
+use super::{NETWORK, PARALLEL_REQUESTS, STOP_GAP};
+use bdk_esplora::esplora_client::{BlockingClient, Builder};
+use bdk_esplora::EsploraExt;
 use bdk_wallet::bitcoin::bip32::Xpriv;
+use bdk_wallet::chain::spk_client::{
+    FullScanRequestBuilder, FullScanResponse, SyncRequestBuilder, SyncResponse,
+};
+use bdk_wallet::descriptor::{Descriptor, DescriptorPublicKey};
 use bdk_wallet::keys::bip39::Mnemonic;
+use bdk_wallet::keys::DescriptorSecretKey;
+use bdk_wallet::rusqlite::Connection;
 use bdk_wallet::template::{Bip86, DescriptorTemplate};
-use bdk_wallet::KeychainKind;
+use bdk_wallet::{AddressInfo, Wallet};
+use bdk_wallet::{KeychainKind, PersistedWallet};
+use std::collections::BTreeMap;
 
 pub fn wallet_descriptor_from_mnemonic(seed_phrase: &str) -> [u8; 64] {
     let mnemonic = Mnemonic::parse(seed_phrase).expect("Seed phrase");
@@ -10,7 +20,37 @@ pub fn wallet_descriptor_from_mnemonic(seed_phrase: &str) -> [u8; 64] {
     seed
 }
 
-pub fn create_descriptor(seed: [u8; 64]) -> (String, String) {
+pub struct WalletDescriptor {
+    pub external: Descriptor<DescriptorPublicKey>,
+    pub ext_key_map: BTreeMap<DescriptorPublicKey, DescriptorSecretKey>,
+    pub internal: Descriptor<DescriptorPublicKey>,
+    pub int_key_map: BTreeMap<DescriptorPublicKey, DescriptorSecretKey>,
+}
+
+impl WalletDescriptor {
+    pub fn new(
+        external: Descriptor<DescriptorPublicKey>,
+        ext_key_map: BTreeMap<DescriptorPublicKey, DescriptorSecretKey>,
+        internal: Descriptor<DescriptorPublicKey>,
+        int_key_map: BTreeMap<DescriptorPublicKey, DescriptorSecretKey>,
+    ) -> Self {
+        Self {
+            external,
+            ext_key_map,
+            internal,
+            int_key_map,
+        }
+    }
+
+    pub fn descriptor_string(&self) -> (String, String) {
+        (
+            self.external.to_string_with_secret(&self.ext_key_map),
+            self.internal.to_string_with_secret(&self.int_key_map),
+        )
+    }
+}
+
+pub fn create_descriptor(seed: [u8; 64]) -> WalletDescriptor {
     let xprv: Xpriv = Xpriv::new_master(NETWORK, &seed).expect("master key");
     println!("master private key {}", xprv);
 
@@ -20,8 +60,62 @@ pub fn create_descriptor(seed: [u8; 64]) -> (String, String) {
     let (change_descriptor, change_key_map, _) = Bip86(xprv, KeychainKind::Internal)
         .build(NETWORK)
         .expect("internal descriptor");
-    (
-        descriptor.to_string_with_secret(&key_map),
-        change_descriptor.to_string_with_secret(&change_key_map),
-    )
+    WalletDescriptor::new(descriptor, key_map, change_descriptor, change_key_map)
+}
+
+pub fn get_wallet(
+    wallet_descriptor: WalletDescriptor,
+    db_path: &str,
+) -> PersistedWallet<Connection> {
+    let mut connection = Connection::open(db_path).expect("dbpath");
+    let (priv_ext, priv_int) = wallet_descriptor.descriptor_string();
+    let wallet_opt = Wallet::load()
+        .descriptor(KeychainKind::External, Some(priv_ext.clone()))
+        .descriptor(KeychainKind::Internal, Some(priv_int.clone()))
+        .extract_keys()
+        .check_network(NETWORK)
+        .load_wallet(&mut connection)
+        .expect("wallet opt created");
+
+    let (mut wallet, is_new_wallet) = if let Some(load_wallet) = wallet_opt {
+        (load_wallet, false)
+    } else {
+        (
+            Wallet::create(priv_ext, priv_int)
+                .network(NETWORK)
+                .create_wallet(&mut connection)
+                .expect("persisted wallet"),
+            true,
+        )
+    };
+
+    let client: BlockingClient = Builder::new("https://mutinynet.com/api").build_blocking();
+
+    match is_new_wallet {
+        true => {
+            let full_scan_request: FullScanRequestBuilder<KeychainKind> = wallet.start_full_scan();
+            let update: FullScanResponse<KeychainKind> = client
+                .full_scan(full_scan_request, STOP_GAP, PARALLEL_REQUESTS)
+                .expect("full scan");
+            wallet.apply_update(update).unwrap();
+        }
+        false => {
+            let sync_request: SyncRequestBuilder<(KeychainKind, u32)> =
+                wallet.start_sync_with_revealed_spks();
+            let update: SyncResponse = client
+                .sync(sync_request, PARALLEL_REQUESTS)
+                .expect("create udpate");
+            wallet.apply_update(update).unwrap();
+        }
+    }
+
+    wallet
+}
+
+pub fn get_address(wallet: &mut PersistedWallet<Connection>, db_path: &str) -> AddressInfo {
+    let address: AddressInfo = wallet.reveal_next_address(KeychainKind::External);
+
+    let mut connection = Connection::open(db_path).expect("dbpath");
+    wallet.persist(&mut connection).expect("good persist");
+    address
 }
