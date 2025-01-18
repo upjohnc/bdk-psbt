58a30feae39cd4157d06f6784c2f3ebf57330b18
freedom-pep <freedom-pep@proton.me>
2025-01-18 14:33:43 -0600

    Clean up main after functioning psbt created

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod utils;
 use bdk_esplora::esplora_client::{BlockingClient, Builder};
-use bdk_wallet::bitcoin::{psbt::Input, Address, Amount, Network, Weight};
+use bdk_wallet::bitcoin::{Address, Amount, Network, Weight};
 use bdk_wallet::{KeychainKind, SignOptions};
 use std::str::FromStr;
 
@@ -18,65 +18,16 @@ fn main() {
     println!("Start\n");
     let seed_1 = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_1);
     let wallet_descriptor = utils::create_descriptor(seed_1);
-    let (ext_descriptor, int_descriptor) = wallet_descriptor.descriptor_string();
-
-    // println!(
-    //     "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\n",
-    //     ext_descriptor, int_descriptor,
-    // );
     let mut wallet_1 = utils::get_wallet(wallet_descriptor, DB_PATH_1);
-    // let mut builder_1 = wallet_1.build_tx();
-    // let address = Address::from_str("tb1qd28npep0s8frcm3y7dxqajkcy2m40eysplyr9v")
-    //     .unwrap()
-    //     .require_network(Network::Signet)
-    //     .unwrap();
-
-    // let send_amount: Amount = Amount::from_sat(5000);
-    // builder_1.add_recipient(address.script_pubkey(), send_amount);
-
-    // let mut psbt_1 = builder_1.finish().expect("psbt_1");
-    // dbg!(psbt_1.unsigned_tx);
-
-    // // println!("address: {}", utils::get_address(&mut wallet_1, DB_PATH_1).address);
-    // println!("wallet amount: {}", wallet_1.balance().total().to_sat());
-    // for i in wallet_1.list_unspent() {
-    //     println!("{:?}", i);
-    // }
 
     let seed_2 = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_2);
     let wallet_descriptor = utils::create_descriptor(seed_2);
-    let (ext_descriptor, int_descriptor) = wallet_descriptor.descriptor_string();
 
-    // println!(
-    //     "----------------  Descriptors  ------------------------------\nPrivate Key, External:\n{:?}\nPrivate Key, Internal:\n{:?}\n",
-    //     ext_descriptor, int_descriptor,
-    // );
-    let mut wallet_2 = utils::get_wallet(wallet_descriptor, DB_PATH_2);
-    // let mut builder_2 = wallet_2.build_tx();
+    let wallet_2 = utils::get_wallet(wallet_descriptor, DB_PATH_2);
     let address = Address::from_str("tb1qd28npep0s8frcm3y7dxqajkcy2m40eysplyr9v")
         .unwrap()
         .require_network(Network::Signet)
         .unwrap();
-
-    let send_amount: Amount = Amount::from_sat(5000);
-    // builder_2.add_recipient(address.script_pubkey(), send_amount);
-
-    // let mut psbt_2 = builder_2.finish().expect("psbt_2");
-    // dbg!(psbt_2.unsigned_tx);
-    // println!(
-    //     "address: {}",
-    //     utils::get_address(&mut wallet_2, DB_PATH_2).address
-    // );
-    // println!("wallet amount: {}", wallet_2.balance().total().to_sat());
-    // let seed_1 = utils::wallet_descriptor_from_mnemonic(SEED_PHRASE_1);
-    // let wallet_descriptor = utils::create_descriptor(seed_1);
-    // let mut wallet_1 = utils::get_wallet(wallet_descriptor, DB_PATH_1);
-    // let i = wallet_1.list_unspent().next().expect("one item at least");
-    // // // for i in wallet_2.list_unspent() {
-    // let mut t = Input::default();
-
-    // t.witness_utxo = Some(i.txout);
-    // let z = i.outpoint;
 
     let utxo = wallet_2.list_unspent().next().unwrap();
     let psbt_input = wallet_2.get_psbt_input(utxo.clone(), None, false).unwrap();
@@ -95,17 +46,6 @@ fn main() {
         .add_recipient(address.script_pubkey(), Amount::from_sat(35_000))
         .add_foreign_utxo(utxo.outpoint, psbt_input, foreign_utxo_satisfaction)
         .unwrap();
-    // let w = builder_2
-    //     .add_foreign_utxo(z, t, Weight::ZERO)
-    //     .expect("bulding well");
-    // builder_2
-    // .add_recipient(address.script_pubkey(), send_amount)
-    // let mut builder = wallet1.build_tx();
-    // builder_2
-    //     .add_recipient(address.script_pubkey(), send_amount)
-    //     .add_foreign_utxo(z, t, Weight::ZERO)
-    //     // .add_foreign_utxo(utxo.outpoint, psbt_input, foreign_utxo_satisfaction)
-    //     .unwrap();
     let mut psbt = builder.finish().expect("finished psbt build");
     let _ = wallet_2
         .sign(&mut psbt, SignOptions::default())
@@ -117,20 +57,8 @@ fn main() {
 
     let tx = psbt.extract_tx().expect("extract the transaction");
     let client: BlockingClient = Builder::new("https://mutinynet.com/api").build_blocking();
-    let x = client.broadcast(&tx);
+    let _ = client.broadcast(&tx);
     dbg!(tx.compute_txid());
-    dbg!(x.unwrap());
 
-    // println!("{:?}", z);
-    //     // println!("{:?}", i.outpoint);
-    // println!(
-    //     "{:?}",
-    //     t,
-    //     // Input {
-    //     //     witness_utxq: Some(i.txout)
-    //     // }
-    // );
-    //     // println!("{:?}", i);
-    // }
     println!("\nEnd");
 }
